d392b14e2c884d3ec886fe4f2e8efe0544a9d675
KerryYang <yys199605@outlook.com>
2020-08-11 16:48:23 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import x from "./x.js";
+import xx from "./x.css";
 console.log(x);
 // console.log("xxx");
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,23 @@
-module.exports = { mode: "development" };
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
